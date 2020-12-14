c74350ff56240adf0f746ae1c488e2a66bcc0ac4
pro100Sardor <sardorbek.juraqulov@gmail.com>
2020-12-14 14:30:30 +0500

    the task assigned to the project has been completed, the code optimization process is underway

--- js/date.js
+++ js/date.js
@@ -6,40 +6,49 @@ const students = [
   {id: 5, firstName: 'Ilhom', lastName: 'Shamidinov'}
 ]
 
-const studentsSchoolInfo = [
-  {
-    id: 1,
-    studentId: 1,
-    dateOfTheLesson: '2020-12-01',
-    studentScore: 5,
-    isStudentAttendLesson: true
-  },
-  {
-    id: 1,
-    studentId: 1,
-    dateOfTheLesson: '2020-12-01',
-    studentScore: 5,
-    isStudentAttendLesson: true
-  },
-  {
-    id: 1,
-    studentId: 1,
-    dateOfTheLesson: '2020-12-01',
-    studentScore: 5,
-    isStudentAttendLesson: true
-  },
-  {
-    id: 1,
-    studentId: 1,
-    dateOfTheLesson: '2020-12-01',
-    studentScore: 5,
-    isStudentAttendLesson: true
-  },
-  {
-    id: 1,
-    studentId: 1,
-    dateOfTheLesson: '2020-12-01',
-    studentScore: 5,
-    isStudentAttendLesson: true
-  }
+const studentsInfo = [
+  // first student info
+  {id: 1, studentId: 1, dateOfTheLesson: '2020-12-01', studentScore: 5, wasStudentAttendLesson: true},
+  {id: 2, studentId: 1, dateOfTheLesson: '2020-12-02', studentScore: 4, wasStudentAttendLesson: true},
+  {id: 3, studentId: 1, dateOfTheLesson: '2020-12-03', studentScore: 5, wasStudentAttendLesson: true},
+  {id: 4, studentId: 1, dateOfTheLesson: '2020-12-04', studentScore: 4, wasStudentAttendLesson: true},
+  {id: 5, studentId: 1, dateOfTheLesson: '2020-12-07', studentScore: 4, wasStudentAttendLesson: true},
+  {id: 6, studentId: 1, dateOfTheLesson: '2020-12-08', studentScore: 4, wasStudentAttendLesson: true},
+  {id: 7, studentId: 1, dateOfTheLesson: '2020-12-09', studentScore: 5, wasStudentAttendLesson: true},
+
+  // second student info
+  {id: 8, studentId: 2, dateOfTheLesson: '2020-12-01', studentScore: 0, wasStudentAttendLesson: false},
+  {id: 9, studentId: 2, dateOfTheLesson: '2020-12-02', studentScore: 3, wasStudentAttendLesson: true},
+  {id: 10, studentId: 2, dateOfTheLesson: '2020-12-03', studentScore: 0, wasStudentAttendLesson: false},
+  {id: 11, studentId: 2, dateOfTheLesson: '2020-12-04', studentScore: 0, wasStudentAttendLesson: false},
+  {id: 12, studentId: 2, dateOfTheLesson: '2020-12-07', studentScore: 4, wasStudentAttendLesson: true},
+  {id: 13, studentId: 2, dateOfTheLesson: '2020-12-08', studentScore: 2, wasStudentAttendLesson: true},
+  {id: 14, studentId: 2, dateOfTheLesson: '2020-12-09', studentScore: 0, wasStudentAttendLesson: false},
+
+  // third student info
+  {id: 15, studentId: 3, dateOfTheLesson: '2020-12-01', studentScore: 5, wasStudentAttendLesson: true},
+  {id: 16, studentId: 3, dateOfTheLesson: '2020-12-02', studentScore: 0, wasStudentAttendLesson: false},
+  {id: 17, studentId: 3, dateOfTheLesson: '2020-12-03', studentScore: 5, wasStudentAttendLesson: true},
+  {id: 18, studentId: 3, dateOfTheLesson: '2020-12-04', studentScore: 0, wasStudentAttendLesson: false},
+  {id: 19, studentId: 3, dateOfTheLesson: '2020-12-07', studentScore: 5, wasStudentAttendLesson: true},
+  {id: 20, studentId: 3, dateOfTheLesson: '2020-12-08', studentScore: 4, wasStudentAttendLesson: true},
+  {id: 21, studentId: 3, dateOfTheLesson: '2020-12-09', studentScore: 0, wasStudentAttendLesson: false},
+
+  // fourth student info
+  {id: 22, studentId: 4, dateOfTheLesson: '2020-12-01', studentScore: 5, wasStudentAttendLesson: true},
+  {id: 23, studentId: 4, dateOfTheLesson: '2020-12-02', studentScore: 4, wasStudentAttendLesson: true},
+  {id: 24, studentId: 4, dateOfTheLesson: '2020-12-03', studentScore: 5, wasStudentAttendLesson: true},
+  {id: 25, studentId: 4, dateOfTheLesson: '2020-12-04', studentScore: 4, wasStudentAttendLesson: true},
+  {id: 26, studentId: 4, dateOfTheLesson: '2020-12-07', studentScore: 4, wasStudentAttendLesson: true},
+  {id: 27, studentId: 4, dateOfTheLesson: '2020-12-08', studentScore: 0, wasStudentAttendLesson: false},
+  {id: 28, studentId: 4, dateOfTheLesson: '2020-12-09', studentScore: 0, wasStudentAttendLesson: false},
+
+  // fifth student info
+  {id: 29, studentId: 5, dateOfTheLesson: '2020-12-01', studentScore: 0, wasStudentAttendLesson: false},
+  {id: 30, studentId: 5, dateOfTheLesson: '2020-12-02', studentScore: 4, wasStudentAttendLesson: true},
+  {id: 31, studentId: 5, dateOfTheLesson: '2020-12-03', studentScore: 0, wasStudentAttendLesson: false},
+  {id: 32, studentId: 5, dateOfTheLesson: '2020-12-04', studentScore: 3, wasStudentAttendLesson: true},
+  {id: 33, studentId: 5, dateOfTheLesson: '2020-12-07', studentScore: 5, wasStudentAttendLesson: true},
+  {id: 34, studentId: 5, dateOfTheLesson: '2020-12-08', studentScore: 4, wasStudentAttendLesson: true},
+  {id: 35, studentId: 5, dateOfTheLesson: '2020-12-09', studentScore: 0, wasStudentAttendLesson: false},
 ]
--- js/main.js
+++ js/main.js
@@ -1,14 +1,57 @@
-let elStudent = document.querySelector('#studentsSelector');
+let elStudents = document.querySelector('#studentsSelector');
+let elStudentName = document.querySelector('#studentName');
+let elStudentInfoTable = document.querySelector('#studentInfoTable');
+let elStudentInfoTableBody = elStudentInfoTable.querySelector('#js-student-info-table__body');
+let elStudentInfoTableTemplate = document.querySelector('#studentInfoTableTemplate').content;
 
 let studentSelectorFragment = document.createDocumentFragment();
 
 students.forEach(student => {
   let studentOption = document.createElement('option');
+
+  studentOption.classList.add("page-main__students-option");
   studentOption.value = student.id;
   studentOption.textContent = `${student.firstName} ${student.lastName}`;
+
   studentSelectorFragment.appendChild(studentOption);
 });
 
-elStudent.appendChild(studentSelectorFragment);
+elStudents.appendChild(studentSelectorFragment);
+
+function studentSelector(evt) {
+  elStudentName.textContent = '';
+  elSelectedStudent = students.find(student => student.id === Number(elStudents.value));
+  elStudentName.textContent = `${elSelectedStudent.firstName} ${elSelectedStudent.lastName}'s information`;
+
+  elStudentInfoTableBody.innerHTML = '';
+
+  let selectedStudentInfoFragment = document.createDocumentFragment();
+
+  studentsInfo.forEach(studentInfos => {
+
+    if (studentInfos.studentId === Number(elStudents.value)) {
+
+      let studentInfoTableRow = document.createElement('tr');
+
+      for(let studentInfo in studentInfos) {
+        if(studentInfo === 'id' || studentInfo === 'studentId') {
+          continue;
+        }
+        let studentInfoTableDate = document.createElement('td');
+        studentInfoTableDate.textContent = studentInfos[studentInfo];
+        studentInfoTableRow.appendChild(studentInfoTableDate);
+      }
+
+      selectedStudentInfoFragment.appendChild(studentInfoTableRow);
+    }
+  });
+
+  elStudentInfoTableBody.appendChild(selectedStudentInfoFragment);
+
+  elStudentInfoTable.classList.remove("d-none");
+}
+
+elStudents.addEventListener('change', studentSelector);
+
 
 
